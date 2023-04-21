7174b36dbebe5bb90de7f7dbf701e76fbfeaa28d
Crystal1921 <1921331938@qq.com>
2023-04-21 19:54:56 +0800

    add background music
    add background music

--- src/util/MP3Player.java
+++ src/util/MP3Player.java
@@ -23,6 +23,10 @@ public class MP3Player {
         player.play();
     }
 
+    public void setFilename (String filename) {
+        this.filename = filename;
+    }
+
     public void stop() {
         player.stop();
     }
